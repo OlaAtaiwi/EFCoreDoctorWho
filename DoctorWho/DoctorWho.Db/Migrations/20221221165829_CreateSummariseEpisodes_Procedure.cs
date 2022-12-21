6b4d6b0f17980320035ab687ff9601f0fc92d266
Ola Alataiwi <olaabilal1@gmail.com>
2022-12-21 19:03:14 +0200

    Create functions and stored procedures

---
+++ DoctorWho/DoctorWho.Db/Migrations/20221221165829_CreateSummariseEpisodes_Procedure.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Db.Migrations
+{
+    public partial class CreateSummariseEpisodes_Procedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE OR ALTER PROCEDURE spSummariseEpisodes AS
+                BEGIN
+	            SELECT TOP 3 c.CompanionName AS [Most Frequent Companions]
+	            FROM EpisodeCompanions e
+	            JOIN Companions c ON e.CompanionId = c.CompanionId
+	            GROUP BY c.CompanionName
+	            ORDER BY COUNT(e.CompanionId) DESC
+
+	            SELECT TOP 3 e.EnemyName AS [Most Frequent Enemies]
+	            FROM Enemies e
+	            JOIN EpisodeEnemies ep ON e.EnemyId = ep.EnemyId
+	            GROUP BY e.EnemyName
+	            ORDER BY COUNT(ep.EnemyID) DESC
+                END;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE spSummariseEpisodes");
+        }
+    }
+}
