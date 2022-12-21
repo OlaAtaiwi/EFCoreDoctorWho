24b380857f0ebecc164d8db40681aa4546a59057
Ola Alataiwi <olaabilal1@gmail.com>
2022-12-21 21:54:55 +0200

    Solving (AddEnemyToEpisode\AddCompanionToEpisode\ GetAllDoctors\GetEnemyById\GetCompanionById)

--- DoctorWho/DoctorWho/Program.cs
+++ DoctorWho/DoctorWho/Program.cs
@@ -3,3 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 
 Console.WriteLine("Hello, World!");
+
+AddEnemyToEpisode(1, 5);
+void AddCompanionToEpisode(int episodeId,int companionId)
+{
+    var episode = DoctorWhoCoreDbContext._context.Episodes.Find(episodeId);
+    if(episode!=null)
+    {
+        episode.EpisodeCompanions.Add(new EpisodeCompanion { CompanionId=companionId, EpisodeId=episodeId });
+        DoctorWhoCoreDbContext._context.SaveChanges();
+    }
+}
+
+void AddEnemyToEpisode(int episodeId, int enemyId)
+{
+    var episode = DoctorWhoCoreDbContext._context.Episodes.Find(episodeId);
+    if(episode!= null)
+    {
+        episode.EpisodeEnemies.Add(new EpisodeEnemy { EnemyId=enemyId, EpisodeId=episodeId });
+        DoctorWhoCoreDbContext._context.SaveChanges();
+    }
+}
